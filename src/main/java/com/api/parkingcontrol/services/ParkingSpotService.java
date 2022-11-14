526ea0439b30722ca62bb1c2bd93096435518f44
Roberto Araripe <roberto.araripe@outlook.com>
2022-11-14 11:51:33 -0300

    Classes essenciais criadas.

---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,14 @@
+package com.api.parkingcontrol.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.repositorys.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+	
+	@Autowired
+	ParkingSpotRepository parkingSpotRepository;
+
+}
